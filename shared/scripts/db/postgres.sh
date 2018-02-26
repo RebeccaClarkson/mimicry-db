d95ccaaf0775e92ade23b082be6119adbf3acee4
Rebecca Clarkson <Rebecca.L.Clarkson@gmail.com>
2018-02-25 16:51:20 -0800

    Fixed postgres loading script

--- shared/scripts/db/postgres.sh
+++ shared/scripts/db/postgres.sh
@@ -1,2 +1,2 @@
 /etc/init.d/postgresql start
-psql -U pguser -h localhost -d mimicry -f shared/scripts/db/buildmimic.sql
+psql -U pguser -h localhost -d mimicry -f /analysis/shared/scripts/db/buildmimic.sql
