82102556ad65cff595f5be039a03a6fb02cd93c1
Rebecca Clarkson <Rebecca.L.Clarkson@gmail.com>
2018-02-25 17:11:29 -0800

    Added column "has_matched_waveform" to both patient and admissions tables

---
+++ shared/scripts/db/add_has_matched_waveform.sql
@@ -0,0 +1,2 @@
+ALTER TABLE patients ADD has_matched_waveform boolean;
+ALTER TABLE admissions ADD has_matched_waveform boolean;
--- shared/scripts/db/postgres.sh
+++ shared/scripts/db/postgres.sh
@@ -1,2 +1,3 @@
 /etc/init.d/postgresql start
 psql -U pguser -h localhost -d mimicry -f /analysis/shared/scripts/db/buildmimic.sql
+psql -U pguser -h localhost -d mimicry -f /analysis/shared/scripts/db/add_has_matched_waveform.sql
