e9da070f5148559d33958b5e1f32a5e7c1f881df
Rebecca Clarkson <Rebecca.L.Clarkson@gmail.com>
2018-02-25 16:18:21 -0800

    Fixed script to build docker container. Added loading fake mimic db to file initializing postgres

--- container/run.sh
+++ container/run.sh
@@ -1,2 +1,2 @@
-docker build -t mimic build/.
+docker build -t mimic container/.
 docker run -it --name mimic_db -p 8000:8000 -v `pwd`/shared:/analysis/shared mimic
--- shared/scripts/db/postgres.sh
+++ shared/scripts/db/postgres.sh
@@ -1,1 +1,2 @@
 /etc/init.d/postgresql start
+psql -U pguser -h localhost -d mimicry -f shared/scripts/db/buildmimic.sql
