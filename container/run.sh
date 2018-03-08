b48f637019edd383861d4fe78ceca1a853b1c7b9
Rebecca Clarkson <Rebecca.L.Clarkson@gmail.com>
2018-03-08 08:43:38 -0800

    Changed port

--- container/run.sh
+++ container/run.sh
@@ -1,2 +1,2 @@
 docker build -t mimicry_db container/.
-docker run -it --name mimicry_db -p 8000:8000 -v `pwd`/shared:/analysis/shared mimic
+docker run -it --name mimicry_db -p 8282:8282 -v `pwd`/shared:/analysis/shared mimic
