57be22f7d2b5bb926e3d7554c4b4e9254ee4151a
Rebecca Clarkson <Rebecca.L.Clarkson@gmail.com>
2018-03-08 09:47:11 -0800

    Changed mimic --> mimicry_db within container/run.sh

--- container/run.sh
+++ container/run.sh
@@ -1,2 +1,2 @@
 docker build -t mimicry_db container/.
-docker run -it --name mimicry_db -p 8282:8282 -v `pwd`/shared:/analysis/shared mimic
+docker run -it --name mimicry_db -p 8282:8282 -v `pwd`/shared:/analysis/shared mimicry_db
